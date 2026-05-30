0833591cb2a884fe4e4cc1e1b01904b3eac279db
jamaleko <60812791+jamaleko@users.noreply.github.com>
2026-05-30 09:26:06 +0700

    Update client.go

--- internal/sofascore/client.go
+++ internal/sofascore/client.go
@@ -20,9 +20,10 @@ func New() *Client {
 func (c *Client) ScheduledEvents(date string) ([]Event, error) {
 
     url := fmt.Sprintf(
-        "https://www.sofascore.com/api/v1/sport/football/scheduled-events/%s",
+        "https://www.sofascore.com/api/v1/event/12437786",
         date,
     )
+    fmt.Println("url:", url)
 
     req, err := http.NewRequest("GET", url, nil)
     if err != nil {

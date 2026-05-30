74bae04c19b1dd73530f54fb4dc0692101318ae0
jamaleko <60812791+jamaleko@users.noreply.github.com>
2026-05-30 11:49:54 +0700

    Update client.go

--- internal/footballdata/client.go
+++ internal/footballdata/client.go
@@ -16,12 +16,39 @@ func New(token string) *Client {
 		http:  &http.Client{},
 	}
 }
-
-func (c *Client) PremierLeagueMatches() ([]Match, error) {
+func (c *Client) WorldCupMatches() ([]Match, error) {
 
 	req, err := http.NewRequest(
 		"GET",
-		"https://api.football-data.org/v4/matches/537327",
+		"https://api.football-data.org/v4/competitions/WC/matches?status=SCHEDULED",
+		nil,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("X-Auth-Token", c.token)
+
+	resp, err := c.http.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	var result MatchesResponse
+
+	err = json.NewDecoder(resp.Body).Decode(&result)
+	if err != nil {
+		return nil, err
+	}
+
+	return result.Matches, nil
+}
+func (c *Client) ChampionsLeagueMatches() ([]Match, error) {
+
+	req, err := http.NewRequest(
+		"GET",
+		"https://api.football-data.org/v4/competitions/CL/matches?status=SCHEDULED",
 		nil,
 	)
 	if err != nil {

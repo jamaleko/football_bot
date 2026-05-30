3f900a10cecce57ba4bc3d1d8d8bc9b1a0109ede
jamaleko <60812791+jamaleko@users.noreply.github.com>
2026-05-30 11:55:09 +0700

    Update main.go

--- cmd/test/main.go
+++ cmd/test/main.go
@@ -10,22 +10,17 @@ func main() {
 
 	client := footballdata.New("40d8ae2308d148afa834e6253fab62fd")
 
-	matches, err := client.PremierLeagueMatches()
+	matches, err := client.WorldCupMatches()
 	if err != nil {
 		panic(err)
 	}
 
-	for i, m := range matches {
-
-		if i >= 10 {
-			break
-		}
-
-		fmt.Printf(
-			"%d | %s vs %s\n",
-			m.ID,
-			m.HomeTeam.Name,
-			m.AwayTeam.Name,
-		)
+	for _, m := range matches {
+	    fmt.Printf(
+	        "%d | %s vs %s\n",
+	        m.ID,
+	        m.HomeTeam.Name,
+	        m.AwayTeam.Name,
+	    )
 	}
 }

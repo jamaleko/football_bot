f2b824ec6710ede7c7a5724bec247b3961d8adc9
jamaleko <60812791+jamaleko@users.noreply.github.com>
2026-05-30 09:08:52 +0700

    Update main.go

--- cmd/test/main.go
+++ cmd/test/main.go
@@ -8,16 +8,16 @@ import (
 )
 
 func main() {
-
+    
     client := sofascore.New()
 
     today := time.Now().Format("2006-01-02")
-
+    fmt.Println("START")
     events, err := client.ScheduledEvents(today)
     if err != nil {
         panic(err)
     }
-
+    fmt.Println("CALL DONE")
     for _, e := range events {
 
         fmt.Printf(

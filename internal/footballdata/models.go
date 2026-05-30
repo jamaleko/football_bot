a6cc8442fe8d11ac064e784e6267c64032f0de73
jamaleko <60812791+jamaleko@users.noreply.github.com>
2026-05-30 17:22:03 +0700

    Update models.go

--- internal/footballdata/models.go
+++ internal/footballdata/models.go
@@ -10,9 +10,18 @@ type Match struct {
 	UTCDate string `json:"utcDate"`
 	HomeTeam Team `json:"homeTeam"`
 	AwayTeam Team `json:"awayTeam"`
+	Score Score `json:"score"`
 }
 
 type Team struct {
 	ID   int    `json:"id"`
 	Name string `json:"name"`
+}
+type Score struct {
+ FullTime FullTime `json:"fullTime"`
+}
+
+type FullTime struct {
+ Home *int `json:"home"`
+ Away *int `json:"away"`
 }

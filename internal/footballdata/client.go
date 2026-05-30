8e17096ec35613f742cfc6b027206e1b82492fae
jamaleko <60812791+jamaleko@users.noreply.github.com>
2026-05-30 10:57:38 +0700

    Update client.go

--- internal/footballdata/client.go
+++ internal/footballdata/client.go
@@ -1,17 +1,29 @@
 package footballdata
 
-import (
-	"net/http"
-)
-
-type Client struct {
-	token string
-	http  *http.Client
+type MatchesResponse struct {
+	Matches []Match `json:"matches"`
 }
 
-func New(token string) *Client {
-	return &Client{
-		token: token,
-		http:  &http.Client{},
-	}
+type Match struct {
+	ID       int  `json:"id"`
+	Status   string `json:"status"`
+
+	HomeTeam Team `json:"homeTeam"`
+	AwayTeam Team `json:"awayTeam"`
+
+	Score Score `json:"score"`
+}
+
+type Team struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+type Score struct {
+	FullTime FullTime `json:"fullTime"`
+}
+
+type FullTime struct {
+	Home *int `json:"home"`
+	Away *int `json:"away"`
 }

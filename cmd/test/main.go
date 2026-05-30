90b6e5178fd41ca78128ef8a61a632794063318f
jamaleko <60812791+jamaleko@users.noreply.github.com>
2026-05-30 12:13:15 +0700

    Update main.go

--- cmd/test/main.go
+++ cmd/test/main.go
@@ -8,26 +8,15 @@ import (
 
 func main() {
 
-	client := footballdata.New("40d8ae2308d148afa834e6253fab62fd")
+	client := footballdata.New(os.Getenv("TOKEN"))
 
-	wc, err := client.WorldCupMatches()
+	match, err := client.Match(552096)
 	if err != nil {
 		panic(err)
 	}
-	
-	cl, err := client.ChampionsLeagueMatches()
-	if err != nil {
-		panic(err)
-	}
-	
-	matches := append(cl, wc...)
-	
-	for i, m := range matches {
-		fmt.Printf(
-			"%d | %s vs %s\n",
-			i+1,
-			m.HomeTeam.Name,
-			m.AwayTeam.Name,
-		)
-	}
+
+	fmt.Println(match.ID)
+	fmt.Println(match.Status)
+	fmt.Println(match.HomeTeam.Name)
+	fmt.Println(match.AwayTeam.Name)
 }

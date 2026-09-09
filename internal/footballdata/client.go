98cbeae471a37c0ad956b6859502d3499fa7f08c
jamaleko <60812791+jamaleko@users.noreply.github.com>
2026-09-09 09:54:22 +0700

    Change API endpoints for World Cup and Champions League

--- internal/footballdata/client.go
+++ internal/footballdata/client.go
@@ -21,7 +21,7 @@ func (c *Client) WorldCupMatches() ([]Match, error) {
 
 	req, err := http.NewRequest(
 		"GET",
-		"https://api.football-data.org/v4/competitions/WC/matches?status=SCHEDULED",
+		"https://api.football-data.org/v4/competitions/PL/matches?status=SCHEDULED",
 		nil,
 	)
 	if err != nil {
@@ -49,7 +49,7 @@ func (c *Client) ChampionsLeagueMatches() ([]Match, error) {
 
 	req, err := http.NewRequest(
 		"GET",
-		"https://api.football-data.org/v4/competitions/CL/matches?status=SCHEDULED",
+		"https://api.football-data.org/v4/competitions/PD/matches?status=SCHEDULED",
 		nil,
 	)
 	if err != nil {
